7fd36219a562f74d906743b1f99451a218567e04
Adam Gask <adam.gask@experian.com>
2019-03-11 11:47:42 +0000

    Init

---
+++ app/src/androidTest/java/aj9/co/uk/emptyandroidproject/ExampleInstrumentedTest.kt
@@ -0,0 +1,37 @@
+package aj9.co.uk.emptyandroidproject
+
+import android.support.test.InstrumentationRegistry
+import android.support.test.espresso.Espresso.onView
+import android.support.test.espresso.assertion.ViewAssertions.matches
+import android.support.test.espresso.matcher.ViewMatchers.isDisplayed
+import android.support.test.espresso.matcher.ViewMatchers.withText
+import android.support.test.rule.ActivityTestRule
+import android.support.test.runner.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+import org.junit.Rule
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getTargetContext()
+        assertEquals("aj9.co.uk.emptyandroidproject", appContext.packageName)
+    }
+
+    @get:Rule
+    var rule = ActivityTestRule(MainActivity::class.java)
+
+    fun test() {
+        onView(withText("Hello World!")).check(matches(isDisplayed()))
+    }
+}
